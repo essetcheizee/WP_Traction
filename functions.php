94ae48a270f2e8e315df0ffa45c4dd84fb42aec3
essetcheizee <48406659+essetcheizee@users.noreply.github.com>
2020-07-02 00:29:59 +0800

    push

--- functions.php
+++ functions.php
@@ -15,12 +15,12 @@ define('THEME_DEV_MODE', true);
     add_action('wp_enqueue_scripts', 'ax_enqueue');
     add_action( 'after_setup_theme', 'theme_setup_theme' );
 
-    //Elementor Hooks
-    add_action('elementor/editor/before_enqueue_scripts', 'builder_enqueue_script');
+    // //Elementor Hooks
+    // add_action('elementor/editor/before_enqueue_scripts', 'builder_enqueue_script');
 
 // Shortcodes
 
 //Require
 
 
-    require_once( get_theme_file_path( '/elementor-widgets/widgets-init.php' ) );
+    // require_once( get_theme_file_path( '/elementor-widgets/widgets-init.php' ) );

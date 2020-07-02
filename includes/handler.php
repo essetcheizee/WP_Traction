8f7824b452d9b6dc157007b2ee62ba469e71ba9d
essetcheizee <48406659+essetcheizee@users.noreply.github.com>
2020-07-02 23:19:03 +0800

    push

--- includes/handler.php
+++ includes/handler.php
@@ -8,12 +8,13 @@
  */
 function thewebsitedev_user_social_links( $user_contact ) {
     /* Add user contact methods */
-    $user_contact['facebook'] = __('Facebook Link', 'rebellion');
-    $user_contact['twitter'] = __('Twitter Link', 'rebellion');
-    $user_contact['dribbble'] = __('Dribbble Link', 'rebellion');
-    $user_contact['pinterest'] = __('Pinterest Link', 'rebellion');
-    $user_contact['linkedin'] = __('LinkedIn Link', 'rebellion');
-    $user_contact['googleplus'] = __('Google+ Link', 'rebellion');
+    $user_contact['facebook'] = __('Facebook Link', 'traction');
+    $user_contact['twitter'] = __('Twitter Link', 'traction');
+    $user_contact['dribbble'] = __('Dribbble Link', 'traction');
+    $user_contact['pinterest'] = __('Pinterest Link', 'traction');
+    $user_contact['linkedin'] = __('LinkedIn Link', 'traction');
+    $user_contact['googleplus'] = __('Google+ Link', 'traction');
+    $user_contact['instagram'] = __('Instagram', 'traction');
  
     return $user_contact;
  }
